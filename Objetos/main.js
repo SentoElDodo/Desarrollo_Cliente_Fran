4388c85c453fe96139b1bdabeb5d1a953ae1d744
Sento <vicentxtv49@gmail.com>
2020-10-13 09:02:01 +0200

    sasd

---
+++ Objetos/main.js
@@ -0,0 +1,22 @@
+const coche={
+    modelo:'kuga',
+    marca: 'Ford',
+    fabricacion: 2016,
+    potenciaEnCaballos: 125
+};
+console.log(coche.marca);
+
+const coche2={
+    modelo:'Scenic',
+    marca: 'Renauld',
+    fabricacion: 2003,
+    potenciaEnCaballos: 90
+};
+console.log(coche2);
+
+const coche3={
+    modelo:'C4',
+    marca: 'Citroen',
+    fabricacion: 2019,
+    potenciaEnCaballos: 170
+};
