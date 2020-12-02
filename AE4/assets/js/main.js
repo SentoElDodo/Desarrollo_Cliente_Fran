909ceff2ec625a17002c7991649fa8d78f10761d
Sento <vicentxtv49@gmail.com>
2020-12-02 20:56:41 +0100

    Grafico Funcionando

--- AE4/assets/js/main.js
+++ AE4/assets/js/main.js
@@ -1,20 +1,3 @@
-function ValidaInfo(){
-    let meses=document.getElementById("meses").value;
-    let temperatura=document.getElementById("temperatura").value;
-    let lluvia=document.getElementById("lluvia").value;
-
-    if(meses == "" && temperatura == "" && lluvia == ""){
-        alert("Debes introducir los datos");
-    }else if(meses.lenght == temperatura.lenght && meses.lenght == lluvia.lenght){
-        alert("Son iguales");
-    }
-}
-
-let Enviar= function(){
-    let lstNumero = document.getElementByIdClassName("numero"),
-    
-}
-
 let InfoMeses=[1,2,3,4,5,6,7,8,9,10,11,12];
 let InfoTemperatura=[3,4,3,5,7,10,12,15,12,10,7,5];
 let InfoLluvia=[40,60,110,70,20,15,10,15,90,120,80,50];
@@ -31,15 +14,34 @@ var chart = new Chart(ctx, {
             },
             {
                 label: 'ºC',
-                backgroundColor: 'rgb(255, 99, 132)',
-                borderColor: 'rgb(255, 99, 132)',
+                backgroundColor: 'rgb(89, 35, 250)',
+                borderColor: 'rgb(20, 81, 166)',
                 data: InfoTemperatura
             }
         ],
-        labels: InfoMeses,
-        
+        labels: InfoMeses,    
     },
-    
-    options: {}
 });
+
+function ValidaInfo(){
+    let meses=document.getElementById("meses").value;
+    let temperatura=document.getElementById("temperatura").value;
+    let lluvia=document.getElementById("lluvia").value;
+    let MesesAlmacen=meses.split(",");
+    let TemperaturaAlmacen=temperatura.split(",");
+    let LluviaAlmacen=lluvia.split(",");
+
+    if(meses == "" && temperatura == "" && lluvia == ""){
+        alert("Debes introducir los datos");
+    }else if(meses.lenght == temperatura.lenght && meses.lenght == lluvia.lenght){
+        alert("Datos validos");
+        MesesAlmacen.forEach((element, index)=> {
+            InfoLluvia[element - 1] = LluviaAlmacen[index];
+            InfoTemperatura[element - 1] = TemperaturaAlmacen[index];
+        });
+        chart.update();
+    }else{
+        alert("Introduce campos");
+    }
+}
 
