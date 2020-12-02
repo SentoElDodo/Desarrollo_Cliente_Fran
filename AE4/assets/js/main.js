19600452d90d9d8387bc03bf8c9756e8aa3c2d67
Sento <vicentxtv49@gmail.com>
2020-12-02 19:14:32 +0100

    valida los datos

--- AE4/assets/js/main.js
+++ AE4/assets/js/main.js
@@ -1,19 +1,45 @@
+function ValidaInfo(){
+    let meses=document.getElementById("meses").value;
+    let temperatura=document.getElementById("temperatura").value;
+    let lluvia=document.getElementById("lluvia").value;
+
+    if(meses == "" && temperatura == "" && lluvia == ""){
+        alert("Debes introducir los datos");
+    }else if(meses.lenght == temperatura.lenght && meses.lenght == lluvia.lenght){
+        alert("Son iguales");
+    }
+}
+
+let Enviar= function(){
+    let lstNumero = document.getElementByIdClassName("numero"),
+    
+}
+
+let InfoMeses=[1,2,3,4,5,6,7,8,9,10,11,12];
+let InfoTemperatura=[3,4,3,5,7,10,12,15,12,10,7,5];
+let InfoLluvia=[40,60,110,70,20,15,10,15,90,120,80,50];
+
 var ctx = document.getElementById('myChart').getContext('2d');
 var chart = new Chart(ctx, {
-    // The type of chart we want to create
     type: 'line',
-
-    // The data for our dataset
     data: {
-        labels: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'],
-        datasets: [{
-            label: 'My First dataset',
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [0, 10, 5, 2, 20, 30, 45, 70, 3]
-        }]
+        datasets: [
+            {
+                label: 'mm3',
+                type: 'line',
+                data: InfoLluvia
+            },
+            {
+                label: 'ºC',
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: InfoTemperatura
+            }
+        ],
+        labels: InfoMeses,
+        
     },
-
-    // Configuration options go here
+    
     options: {}
 });
+
