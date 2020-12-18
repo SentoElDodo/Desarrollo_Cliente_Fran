18a3b62541b3e5785ddeca6dad74bad475bee2e8
Sento <vicentxtv49@gmail.com>
2020-12-18 13:02:17 +0100

    Clase 18/12/20

--- Ac_Angular2/ANE7/ANE7/src/app/app.component.spec.ts
+++ Ac_Angular2/ANE7/ANE7/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'proyectoAngular1'`, () => {
+  it(`should have as title 'ANE7'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('proyectoAngular1');
+    expect(app.title).toEqual('ANE7');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('proyectoAngular1 app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('ANE7 app is running!');
   });
 });
--- Ac_Angular2/ANE7/ANE7/src/app/app.module.ts
+++ Ac_Angular2/ANE7/ANE7/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductoComponent } from './productos/producto.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ProductoComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
